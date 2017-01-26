8181cf3f5c408baae4966da66a574c333d4a07fb
James Dowdell <jdowdell@gmail.com>
2017-01-26 00:02:38 -0800

    split RomReader into RomReader proper and Rom.  The later will retain the bytes array for the Rom and be editable later

--- llt/cli/Cli.scala
+++ llt/cli/Cli.scala
@@ -1,8 +1,11 @@
 package llt.cli {
   object Cli {
     def main(args: Array[String]) {
-      require(args.length >= 1, "No args in main()! Usage: scala llt.cli.Cli [romfile]")
-      new llt.io.RomReader(args(0)).loadLevels
+      require(args.length >= 2, "No args in main()! Usage: scala llt.cli.Cli [romfile] [newRomFile]")
+      require(args(0) != args(1), "Can't overwrite original ROM; please change output filename")
+      val rom = new llt.io.RomReader(args(0)).loadRom()
+      val levels = rom.loadLevels()
+      0
     }
   }
 }
--- llt/io/RomReader.scala
+++ llt/io/RomReader.scala
@@ -5,30 +5,13 @@ import java.security.MessageDigest
 
 import llt.common.Level
 
-object RomReader {
-  // I don't know what's up with the ROM I have access to, but it's longer and has a different
-  // sha256 than the ones mentioned on datacrystal.
-  final val RomSize = 262160
-  final val RomSha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  final val LevelCount = 50  // lolo1 has 50 levels exactly
-
-  // Magic Numbers from http://datacrystal.romhacking.net/wiki/Adventures_of_Lolo:ROM_map
-  // These are all byte offsets from the beginning of the ROM (so I call them "Pointer")
-  final val RoomDataOffsetPointer = 0x3A6A
-  final val RoomDataLengthPointer = 0x3ACE
-  final val RoomDataBankPointer = 0x3B00
-
-  // Also, in my dump, the room data is much further in than what datacrystal mentions.
-  final val BaseRoomDataPointer = 0x20010
-}
-
 class RomReader (romDataStream: java.io.InputStream) {
   def this(f: File) = this(new java.io.FileInputStream(f))
   def this(s: String) = this(new File(s))
 
   // Call whenever you're afraid you might be over the limit.
   def confirmFinalSizeLargerThan(fileSize: Int) {
-    require(fileSize <= RomReader.RomSize, "File too big to be a rom (" + fileSize + " bytes)")
+    require(fileSize <= Rom.RomSize, "File too big to be a rom (" + fileSize + " bytes)")
   }
 
   // ala https://www.mkyong.com/java/java-sha-hashing-example/
@@ -39,25 +22,9 @@ class RomReader (romDataStream: java.io.InputStream) {
     sb.toString
   }
 
-  def loadLevel(level: Int, romData: Array[Byte]): Level = {
-    require (0 <= level && level < 50)
-    val bankOffset = ( (0xFF & romData(RomReader.RoomDataOffsetPointer + level*2)) |
-                       ((0xFF & romData(RomReader.RoomDataOffsetPointer + level*2 + 1)) << 8))
-    val length = romData(RomReader.RoomDataLengthPointer + level) & 0xFF
-    val bank = romData(RomReader.RoomDataBankPointer + level) & 0xFF
-
-    // TODO(jdowdell) - dedup this against writing back the Level later
-    // decompress room level data from masterOffset + bank*0x1000 + bankOffset
-    val romLevelOffset = RomReader.BaseRoomDataPointer + bank*0x1000 + bankOffset
-    (Level.fromRomByteBlob(romData.slice(romLevelOffset, romLevelOffset+length))
-          .setLevelNumber(level))
-  }
-
-  def loadLevels() = {
+  def loadRom(): Rom = {
     val bytes = loadRomFromStream(romDataStream)
-
-    val levels = (0 to 50).map { i => loadLevel(i, bytes) }
-    0  // TODO(jdowdell) - return Levels object
+    new Rom(bytes)
   }
 
   def loadRomFromStream(inputStream : InputStream): Array[Byte] = {
@@ -76,7 +43,7 @@ class RomReader (romDataStream: java.io.InputStream) {
       // Read the file, until its obvious we have the wrong one or we get all of it.
       // Update sha hash as we go.
       amountRead = inputStream.read(buffer)
-      while (amountRead != -1 && totalRead <= RomReader.RomSize) {
+      while (amountRead != -1 && totalRead <= Rom.RomSize) {
         totalRead += amountRead
         md.update(buffer, 0, amountRead)
         baos.write(buffer, 0, amountRead)
@@ -91,10 +58,10 @@ class RomReader (romDataStream: java.io.InputStream) {
     // TODO(jdowdell) - guide user on common errors, like their .nes is actually a .zip with a .nes
     // inside it.
     val sha256 = digest2HexStr(md)
-    require(totalRead == RomReader.RomSize,
-            "Filesize mismatch: read " + totalRead + " bytes; expected " + RomReader.RomSize)
-    require(sha256 == RomReader.RomSha256, "SHA256 of first " + RomReader.RomSize + " bytes = "
-                                           + sha256 + ", expected " + RomReader.RomSha256)
+    require(totalRead == Rom.RomSize,
+            "Filesize mismatch: read " + totalRead + " bytes; expected " + Rom.RomSize)
+    require(sha256 == Rom.RomSha256, "SHA256 of first " + Rom.RomSize + " bytes = "
+                                     + sha256 + ", expected " + Rom.RomSha256)
 
     // Return bytes read
     baos.toByteArray
