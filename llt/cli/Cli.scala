56c1556b1367e9dea252e4eab1f5bae60d5c69ec
James Dowdell <jdowdell@gmail.com>
2017-01-28 23:18:33 -0800

    newer scalac found an issue in Cli

--- llt/cli/Cli.scala
+++ llt/cli/Cli.scala
@@ -1,11 +1,10 @@
 package llt.cli
 
 object Cli {
-  def main(args: Array[String]) {
+  def main(args: Array[String]) : Unit = {
     require(args.length >= 2, "No args in main()! Usage: scala llt.cli.Cli [romfile] [newRomFile]")
     require(args(0) != args(1), "Can't overwrite original ROM; please change output filename")
     val rom = new llt.io.RomReader(args(0)).loadRom()
     val levels = rom.loadLevels()
-    0
   }
 }
