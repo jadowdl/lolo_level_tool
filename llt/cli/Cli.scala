af39dc0fc71fa9b6426208270f275849f993452f
James Dowdell <jdowdell@gmail.com>
2017-01-29 12:13:02 -0800

    rough draft of level patching and ROM output; in current form, should output rom that plays the same, but may be slightly better level-compressed than the original

--- llt/cli/Cli.scala
+++ llt/cli/Cli.scala
@@ -1,10 +1,23 @@
 package llt.cli
 
+import java.io.{File, FileOutputStream}
+
 object Cli {
   def main(args: Array[String]) : Unit = {
     require(args.length >= 2, "No args in main()! Usage: scala llt.cli.Cli [romfile] [newRomFile]")
     require(args(0) != args(1), "Can't overwrite original ROM; please change output filename")
     val rom = new llt.io.RomReader(args(0)).loadRom()
     val levels = rom.loadLevels()
+
+
+    // TODO - edit levels in some way...
+
+
+    // Write to result ROM file
+    // TODO(jdowdell) - once there's a gui, we probably don't need to write to File like this.
+    rom.patchLevels(levels)
+    val fos = new FileOutputStream(new File(args(1)))
+    fos.write(rom.getRomData)
+    fos.close()
   }
 }
--- llt/io/Rom.scala
+++ llt/io/Rom.scala
@@ -17,9 +17,15 @@ object Rom {
 
   // Also, in my dump, the room data is much further in than what datacrystal mentions.
   final val BaseRoomDataPointer = 0x20010
+
+  // I'm not sure, but I think actually when we go to write the rom back out, we could start
+  // from 0x00 rather than 0x10.  But for now, I write to the same parts of the bank that the
+  // original ROM has level data in.
+  final val MaxBytesPerBank = 0x21000 - BaseRoomDataPointer
 }
 
 class Rom (romData: Array[Byte]){
+  def getRomData() = romData
 
   def loadLevel(level: Int): Level = {
     require (0 <= level && level < 50)
@@ -35,13 +41,52 @@ class Rom (romData: Array[Byte]){
           .setLevelNumber(level))
   }
 
-  def loadLevels(): Any = {
-    val levels = (0 until 50).map { i => loadLevel(i) }
-    levels
+  def loadLevels(): Seq[Level] = {
+    0 until 50 map { i => loadLevel(i) }
   }
 
-  private def doPatchLevels(levels:Seq[Level], dryRun: Boolean) {
+  private def doPatchLevels(levels:Seq[Level], dryRun: Boolean): Unit = {
+    var bank = 0xFF & romData(Rom.RoomDataBankPointer) // start with the same bank the Rom did b4
+    // See the datacrystal explanation.  There are two sequential banks we have access to for level
+    // data; so we can increment the bank number precisely once.
+    var incBank = 0
 
+    var bankOffset = 0
+    var levelNumber = 0
+
+    for (level <- levels) {
+      val data = level.toByteBlob
+
+      // patch in if this is not a test - this is the opposite of loadLevel()
+      if (!dryRun) {
+        require(bankOffset < Rom.MaxBytesPerBank)
+        require(data.length < 0x100)
+
+        // room data  is two bytes per level, LSB first
+        romData(Rom.RoomDataOffsetPointer + levelNumber*2) = (0xFF & bankOffset).toByte
+        romData(Rom.RoomDataOffsetPointer + levelNumber*2 + 1) = (0xFF & (bankOffset>>8)).toByte
+
+        romData(Rom.RoomDataLengthPointer + levelNumber) = (data.length & 0xFF).toByte
+
+        romData(Rom.RoomDataBankPointer + levelNumber) = (bank & 0xFF).toByte
+
+        // write actual room data to bank
+        val romLevelOffset = Rom.BaseRoomDataPointer + bank*0x1000 + bankOffset
+        Array.copy(data, 0, romData, romLevelOffset, data.length)
+      }
+
+      // Update running offsets
+      bankOffset += data.length
+      if (bankOffset >= Rom.MaxBytesPerBank) {
+        bankOffset = 0
+        incBank += 1
+        if (incBank > 1) {
+          throw new IllegalArgumentException("Too many banks required to patch these levels")
+        }
+        bank += 1
+      }
+      levelNumber += 1
+    }
   }
 
   def patchLevels(levels: Seq[Level]) {
